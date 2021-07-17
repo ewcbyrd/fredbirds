af2c686094234aa9ad58753b9ec71c68e80edd44
Scott Byrd <sbyrd1968@verizon.net>
2021-07-17 13:09:12 -0400

    Clean up app component

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -16,42 +16,6 @@ export default class App extends LightningElement {
         this.dynamicCtor = ctor.default;
     }
 
-    handleBirdsClick() {
-        if (this.clubOpen) this.handleClubClick();
-        let classList = this.template.querySelector('.birds').classList;
-        if (this.birdsOpen) {
-            classList.remove('slds-is-open');
-            this.birdsOpen = false;
-        } else {
-            classList.add('slds-is-open');
-            this.birdsOpen = true;
-        }
-    }
-
-    handleClubClick() {
-        if (this.birdsOpen) this.handleBirdsClick();
-        let classList = this.template.querySelector('.club').classList;
-        if (this.clubOpen) {
-            classList.remove('slds-is-open');
-            this.clubOpen = false;
-        } else {
-            classList.add('slds-is-open');
-            this.clubOpen = true;
-        }
-    }
-
-    get homeStyle() {
-        return this.getMenuStyle(this.homeSelected);
-    }
-
-    get birdsStyle() {
-        return this.sightingsSelected || this.hotspotsSelected ? '' : '';
-    }
-
-    getMenuStyle(selected) {
-        return selected ? 'slds-context-bar__item' : 'slds-context-bar__item';
-    }
-
     handleMenuClick(event) {
         if (this.birdsOpen) this.handleBirdsClick();
         if (this.clubOpen) this.handleClubClick();
