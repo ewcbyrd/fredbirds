573df090c7e4c9c4b25e99593a8cd1a6d9db2296
Scott Byrd <sbyrd1968@verizon.net>
2021-08-12 08:02:27 -0400

    Update to news component to use feeds

--- src/modules/my/news/news.js
+++ src/modules/my/news/news.js
@@ -1,43 +1,55 @@
-import { LightningElement, api } from 'lwc';
-import {
-    getNews
-} from 'data/restdbService';
+import { LightningElement } from 'lwc';
+import { getFeed, getNewsFeeds } from 'data/restdbService';
 
 export default class News extends LightningElement {
-
-    @api home = false;
-    @api numRows = 3;
-    loading = false;
+    url;
     news = [];
+    dropdown = 'All About Birds';
+    dropdownOptions;
+    elapsed = 0;
+    interval;
 
     connectedCallback() {
-        this.fetchNews();
-    }
-
-    fetchNews() {
-        const news = sessionStorage.getItem('news');
-        this.loading = true;
-        if (news) {
-            this.news = this.home ? JSON.parse(news).splice(0, this.numRows) : this.news = JSON.parse(news);
-            this.loading = false;
-        } else {
-            getNews()
-            .then((result) => {
-                this.loading = false;
-                let rows = result.length < this.numRows ? result.length : this.numRows;
-                sessionStorage.setItem('news', JSON.stringify(result));
-                this.news = this.home || result.length === 1 ? result.splice(0, rows) : result;
+        getNewsFeeds()
+            .then((response) => {
+                return response.json();
+            })
+            .then((results) => {
+                this.dropdownOptions = results;
+                this.url = results[0].url;
+                this.fetchNews();
             })
             .catch((error) => {
                 console.log(error);
             });
-        }
+    }
+
+    fetchNews() {
+        getFeed(this.url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((results) => {
+                for (let i = 0; i < results.items.length; i++) {
+                    results.items[i].id = i;
+                }
+                this.news = results.items;
+                this.interval = setInterval(() => {
+                    this.elapsed++;
+                }, 60000);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     get birdNews() {
         this.news.forEach((item) => {
-            item.isLink = item.type === 'link';
-        })
+            item.isLink = item.link !== undefined;
+            if (!item.enclosure) {
+                item.enclosure = {};
+            }
+        });
         return this.news;
     }
 
@@ -45,7 +57,40 @@ export default class News extends LightningElement {
         return this.home === 'true';
     }
 
-    handleViewAllClick() {
-        this.dispatchEvent(new CustomEvent('viewall', { detail: 'news', bubbles: true, composed: true }));
+    get numItems() {
+        return this.news.length;
+    }
+
+    get timeElapsed() {
+        const mins = this.elapsed !== 1 ? 'minutes' : 'minute';
+        return `${this.elapsed} ${mins} ago`;
+    }
+
+    handleListClick() {
+        let classes = this.template.querySelector('.list-select').classList;
+        if (classes.contains('slds-is-open')) {
+            classes.remove('slds-is-open');
+        } else {
+            classes.add('slds-is-open');
+        }
+    }
+
+    handleOptionChange(event) {
+        this.elapsed = 0;
+        clearInterval(this.interval);
+        this.template
+            .querySelector('.list-select')
+            .classList.remove('slds-is-open');
+        this.url = this.dropdownOptions.find(
+            (item) => item.title === event.currentTarget.dataset.id
+        ).url;
+        this.dropdown = event.currentTarget.dataset.id;
+        this.fetchNews();
+    }
+
+    refresh() {
+        this.elapsed = 0;
+        clearInterval(this.interval);
+        this.fetchNews();
     }
 }
