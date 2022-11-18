65a1ce9da2b4445787959212d3dc99cc960f64f6
Scott Byrd <sbyrd1968@verizon.net>
2022-11-18 11:12:18 -0500

    Remove comment

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import { createElement } from 'lwc';
 import MyApp from 'my/app';
 
 const app = createElement('my-app', { is: MyApp });
-// eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
